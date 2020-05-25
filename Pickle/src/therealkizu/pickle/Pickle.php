d3f6fe37ca00b9b08485de55ebe21b98451ae34c
TheRealKizu <jajaskie0403@gmail.com>
2020-05-25 19:29:47 +0800

    Added config file and a function to check the specified language used for the plugin

--- Pickle/src/therealkizu/pickle/Pickle.php
+++ Pickle/src/therealkizu/pickle/Pickle.php
@@ -13,17 +13,44 @@ declare(strict_types=1);
 namespace therealkizu\pickle;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Pickle extends PluginBase {
+
+    /** @var Config $config */
+    public $config;
 
     public function onLoad() {
         if (!is_dir($this->getDataFolder())) {
             @mkdir($this->getDataFolder());
         }
+
+        if (!is_file($this->getDataFolder() . "config.yml")) {
+            $this->saveResource("config.yml");
+        }
     }
 
     public function onEnable() {
         $this->getLogger()->info("Pickle is now enabled!");
+
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+
+        $this->checkLanguages($this->config);
+    }
+
+    /**
+     * This checks what language the plugin will be using.
+     *
+     * @param Config $config
+     * @return void
+     */
+    public function checkLanguages(Config $config): void {
+        if (!is_dir($this->getDataFolder() . "languages/")) {
+            @mkdir($this->getDataFolder() . "languages/");
+        }
+
+        $language = $config->get("language", "en_US");
+        //TODO: Finish this.
     }
 
 }
