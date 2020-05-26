8aaa6d340a3efc0fba883c706c5c0c6f705053e7
EncatedLands <65440395+EncatedLands@users.noreply.github.com>
2020-05-26 17:10:23 +0800

    Update ItemManager.php

--- Pickle/src/therealkizu/pickle/items/ItemManager.php
+++ Pickle/src/therealkizu/pickle/items/ItemManager.php
@@ -18,7 +18,9 @@ use pocketmine\item\ItemFactory;
 use therealkizu\pickle\items\Boat;
 use therealkizu\pickle\items\Crossbow;
 use therealkizu\pickle\items\Minecart;
-use therealkizu\pickle\items\Shield;
+use therealkizu\pickle\items\Shield; 
+use therealkizu\pickle\items\Elytra; 
+use therealkizu\pickle\items\GlassBottle;
 
 class ItemManager {
 
@@ -31,6 +33,8 @@ class ItemManager {
         ItemFactory::registerItem(new Crossbow(), true);
         ItemFactory::registerItem(new Minecart(), true);
         ItemFactory::registerItem(new Shield(), true);
+        ItemFactory::registerItem(new Elytra(), true);
+        ItemFactory::registerItem(new GlassBottle(), true);
         Item::initCreativeItems();
     }
 
